39499ffe345a9ee4e8ca28b82365aae6f05c8a44
AnjaliRawke <anjalirawke21@gmail.com>
2022-10-31 16:33:06 +0530

    Added:UC2 Check Equality of Two Lines

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class LineComparison {
 	public static void main(String[] args) {
-	    int x1,x2,y1,y2;
-	    double lengthofLine;
+	    double x1,x2,y1,y2;
+	    double x3,x4,y3,y4;
+	    double lengthofLine1,LengthOfLine2;
 	    Scanner inputvalues = new Scanner(System.in);
+	    //lengthofline1 calculation
 		System.out.println("Enter the value of x1 :");
 		x1 = inputvalues.nextInt();
 		System.out.println("Enter the value of y1 :");
@@ -15,8 +17,25 @@ public class LineComparison {
 		x2 = inputvalues.nextInt();
 		System.out.println("Enter the value of y2 :");
 		y2 = inputvalues.nextInt();
-		lengthofLine = Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
-		System.out.println("The length of a line =" +lengthofLine);
+		lengthofLine1 = Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
+		System.out.println("The length of a line =" +lengthofLine1);
+		
+		//lengthofline2 calculation
+		System.out.println("Enter the value of x3 :");
+		x3 = inputvalues.nextInt();
+		System.out.println("Enter the value of y3 :");
+		y3 = inputvalues.nextInt();
+		System.out.println("Enter the value of x4 :");
+		x4 = inputvalues.nextInt();
+		System.out.println("Enter the value of y4 :");
+		y4 = inputvalues.nextInt();
+		LengthOfLine2 = Math.sqrt(Math.pow(x4-x3,2)+ Math.pow(y4-y3,2));
+		System.out.println("The length of a line =" +LengthOfLine2);
+		
+		if(lengthofLine1 == LengthOfLine2)
+			System.out.println("Length of both lines are equal");
+		else
+			System.out.println("lengths are not equal");
 	}
 	}
 
